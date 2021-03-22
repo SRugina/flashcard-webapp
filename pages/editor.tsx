40ccb95af791c349ee4feb7868b664c2e2945cfa
Sebastian Rugina <dev@srugina.com>
2021-03-22 23:28:59 +0000

    Add collections page to see what data looks like (#11)

---
+++ pages/editor.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Editor from "../components/Editor";
+
+const EditorPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Flashcard Web App</title>
+        <meta name="Description" content="A Flashcard Web App" />
+      </Head>
+      <div className="mx-auto w-full">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
